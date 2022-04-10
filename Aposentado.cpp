ad18c80fe0cf66860c81d6073dba12b880943577
JetStream-Deiv <103295543+JetStream-Deiv@users.noreply.github.com>
2022-04-10 00:50:40 -0300

    Update Aposentado.cpp

--- Aposentado.cpp
+++ Aposentado.cpp
@@ -18,5 +18,12 @@ int main(){
     }
 
     return 0;
-
+    
+//Uma empresa quer verificar se o empregado está qualificado para a aposentadoria ou não. Para estar em condições, um dos seguintes requisitos deve ser satisfeito:
+//-Ter no mínimo 65 anos de idade;
+//-Ter trabalhado no mínimo 30 anos.
+//-Ter no mínimo 60 anos e ter trabalhado no mínimo 25 anos.
+//Com base nas informações acimas, construa um programa que leia: a idade e o tempo de trabalho do funcionário. 
+//O programa deverá imprimir a idade, o tempo de trabalho e a mensagem “Requer aposentadoria” ou “Não requer aposentadoria”.
+    
 }
